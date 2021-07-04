c1acc30cba909237f3eab7e7bb4e68ae48fd17cd
shihpoan <randypunk123@gmail.com>
2021-07-04 18:57:50 +0800

    新增console.log 成功及錯誤

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let linebot = require('linebot');
+let express = require('express');
 
 // 初始化 line bot 需要的資訊，在 Heroku 上的設定的 Config Vars，可參考 Step2
 let bot = linebot({
@@ -11,8 +12,17 @@ let bot = linebot({
 bot.on('message', function (event) {
   // 回覆訊息給使用者 (一問一答所以是回覆不是推送)
   console.log(event);
-  event.reply(`媽我還不會打字啦嗚嗚`);
+  var msg = event.message.text;
+  event.reply(`媽我還不會打字啦嗚嗚`).then((data)=>{
+    console.log(msg);
+  }).catch(error=>{
+      console.log(error);
+  });
 });
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
 
 // Bot 所監聽的 webhook 路徑與 port，heroku 會動態存取 port 所以不能用固定的 port，沒有的話用預設的 port 5000
 bot.listen('/', process.env.PORT || 5000, function () {
