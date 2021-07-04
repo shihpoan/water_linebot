2f3c143227001949fdbe75ab0677eb318ebd8376
shihpoan <randypunk123@gmail.com>
2021-07-04 21:17:26 +0800

    新增PM2.5

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function _bot(){
 function _getJSON(){
     clearTimeout(timer);
     getJSON('https://data.epa.gov.tw/api/v1/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&sort=ImportDate%20desc&format=json', function(error, response){
-        response.forEach(function(e, i){
+        Array.prototype.forEach.call(response,function(e, i){
             pm[i] = [];
             pm[i][0] = e.SiteName;
             pm[i][1] = e['PM2.5'] * 1;
